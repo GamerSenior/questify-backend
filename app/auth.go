992b3f1d02214565a504b509b950e82f45750db4
Giovani Garcia <giovani.abel@supportweb.com.br>
2019-07-15 17:30:24 -0300

    Path changes

---
+++ app/auth.go
@@ -0,0 +1,70 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	u "github.com/GamerSenior/questify-back/utils"
+	"github.com/GamerSenior/questify-back/models"
+)
+
+func writeErrorResponse(w http.ResponseWriter, message string) {
+	response := u.Message(false, message)
+	w.WriteHeader(http.StatusForbidden)
+	w.Header().Add("Content-Type", "application/json")
+	u.Respond(w, response)
+}
+
+// JwtAuthentication explanation
+var JwtAuthentication = func(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		notAuth := []string{"/api/user/new", "/api/user/login"}
+		requestPath := r.URL.Path
+
+		for _, value := range notAuth {
+			if value == requestPath {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		tokenHeader := r.Header.Get("Authorization")
+
+		if tokenHeader == "" {
+			writeErrorResponse(w, "Missing auth token")
+			return
+		}
+
+		splitted := strings.Split(tokenHeader, " ")
+		if len(splitted) != 2 {
+			writeErrorResponse(w, "Invalid/Malformed auth token")
+			return
+		}
+
+		tokenPart := splitted[1]
+		tk := &models.Token{}
+
+		token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("token_password")), nil
+		})
+
+		if err != nil {
+			writeErrorResponse(w, "Malformed authentication token")
+			return
+		}
+
+		if !token.Valid {
+			writeErrorResponse(w, "Token is not valid")
+			return
+		}
+
+		fmt.Printf("User: %v", tk.UserID)
+		ctx := context.WithValue(r.Context(), "user", tk.UserID)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+	})
+}
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/GamerSenior/questify-back/app"
+	"github.com/GamerSenior/questify-back/controllers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.Use(app.JwtAuthentication)
+
+	router.HandleFunc("/api/users/new", controllers.CreateAccount).Methods("POST")
+	router.HandleFunc("/api/users/login", controllers.Authenticate).Methods("POST")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		fmt.Print(err)
+	}
+}
